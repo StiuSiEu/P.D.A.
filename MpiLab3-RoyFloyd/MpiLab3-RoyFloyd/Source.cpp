0d09ac9b41fbf4d8b00afdcaf32693fea17576b7
Tolgoi Gabriel <omunegru34@gmail.com>
2017-04-10 22:16:41 +0300

    Add files via upload

--- MpiLab3-RoyFloyd/MpiLab3-RoyFloyd/Source.cpp
+++ MpiLab3-RoyFloyd/MpiLab3-RoyFloyd/Source.cpp
@@ -2,30 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 5
-
 int main(int argc, char *argv[])
 {
-	int a[N][N] = {
-		{  0,   2, -1, 10 ,-1 },
-		{  2,   0,  3, -1 ,-1 },
-		{ -1,   3,  0,  1 , 8 },
-		{ 10,  -1,  1,  0 ,-1 },
-		{ -1,  -1,  8, -1 , 0 }
+	int i,j,k;
+	int a[5][5] = {
+		{  0,   2, 99, 10 ,99 },
+		{  2,   0,  3, 99 ,99 },
+		{ 99,   3,  0,  1 , 8 },
+		{ 10,  99,  1,  0 ,99 },
+		{ 99,  99,  8, 99 , 0 }
 	};
 	
-	int result[N][N];
+	int costMatrix[5][5];    
+	/*
+	  0  2  5 6 13
+	  2	 0  3 4 11
+	  5  3  0 1  8
+	  6  4  1 0  9
+	  13 11 8 9  0
+	*/
+
 	int numprocs, rank;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 	
-	for (int k = 0; k < N; k++)
+	for (k = 0; k < 5; k++)
 	{
-		for (int i = rank; i < N; i = i + numprocs)
+		for (i = rank; i < 5; i = i + numprocs)
 		{
-			for (int j = 0; j < N; j++)
+			for (j = 0; j < 5; j++)
 			{
 				if (a[i][j] > a[i][k] + a[k][j])
 					
@@ -33,20 +40,29 @@ int main(int argc, char *argv[])
 			}
 		}
 		
+		MPI_Allreduce(a, costMatrix, 25, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 		
-		MPI_Reduce(a, result, N*N, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-		
-		if (rank == 0) {
-			for (int i = 0; i < N; i++)
-				for (int j = 0; j < N; j++)
-					a[i][j] = result[i][j];
-			MPI_Bcast(a, N*N, MPI_INT, 0, MPI_COMM_WORLD);
-		}
+		for (i = 0; i < 5; i++)
+			for (j = 0; j < 5; j++)
+				a[i][j] = costMatrix[i][j];		
 		
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
 	MPI_Finalize();
+
+	if (rank == 0)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			for (j = 0; j < 5; j++)
+			{
+				printf("%d ", costMatrix[i][j]);
+			}
+			printf("\n");
+		}
+	}
+		
 }
 
 
